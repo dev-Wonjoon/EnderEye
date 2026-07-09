ecfea9b77440a56eec453a342d9816fba02a0b14
Dev-Wonjoon <dnjswns775@gmail.com>
2026-07-10 03:55:20 +0900

    Project init

---
+++ src/main/kotlin/net/endereye/EnderEyeApplication.kt
@@ -0,0 +1,11 @@
+package net.endereye
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EnderEyeApplication
+
+fun main(args: Array<String>) {
+    runApplication<EnderEyeApplication>(*args)
+}
---
+++ src/test/kotlin/net/endereye/EnderEyeApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.endereye
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EnderEyeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
